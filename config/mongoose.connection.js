361d02325ac53e3611987740348e02ecb8cd8877
piyush singh tanwar <piyushsinghtomar777@gmail.com>
2026-03-15 15:15:07 +0530

    fix: update MongoDB connection string to use a direct URI

--- config/mongoose.connection.js
+++ config/mongoose.connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dbgr = require("debug")("development:mongoose");
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect("mongodb+srv://piyushsinghtomar777_db_user:2DdpQPJcCnZFJNwf@cluster0.aahupud.mongodb.net/?appName=Cluster0")
   .then(() => {
     dbgr("mongoose connected");
     console.log("mongodb connected");
