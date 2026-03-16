e851e834638bf2d336bd67b14265ba1b9183a77e
piyush singh tanwar <piyushsinghtomar777@gmail.com>
2026-03-16 12:24:10 +0530

    fix: update MongoDB connection to use environment variable

--- config/mongoose.connection.js
+++ config/mongoose.connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dbgr = require("debug")("development:mongoose");
 
 mongoose
-  .connect("mongodb+srv://piyushsinghtomar777_db_user:2DdpQPJcCnZFJNwf@cluster0.aahupud.mongodb.net/?appName=Cluster0")
+  .connect(process.env.MONGODB_URL)
   .then(() => {
     dbgr("mongoose connected");
     console.log("mongodb connected");
